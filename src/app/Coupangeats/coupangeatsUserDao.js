8694cc4d47bd30de9fcd680e77a6c05d1f0ad4c3
hannah100-coder <pd206@naver.com>
2021-12-19 08:44:15 +0900

    still has error

--- src/app/Coupangeats/coupangeatsUserDao.js
+++ src/app/Coupangeats/coupangeatsUserDao.js
@@ -23,7 +23,7 @@ async function insertUserInfo(connection, insertUserInfoParams) {
     return insertUserInfoRow;
 }
 
-<<<<<<< HEAD
+
 //유저 주소 입력
 async function updateUserAddress(connection, userIndex, userAddress) {
     const updateUserAddressQuery = `
@@ -36,44 +36,9 @@ async function updateUserAddress(connection, userIndex, userAddress) {
 }
 
 
-// 패스워드 체크
-async function selectUserPassword(connection, selectUserPasswordParams) {
-    const selectUserPasswordQuery = `
-          SELECT userEmail, userName, userPassword
-          FROM User
-          WHERE userEmail = ? AND userPassword = ?;
-          `;
-    const selectUserPasswordRow = await connection.query(
-        selectUserPasswordQuery,
-        selectUserPasswordParams
-    );
-  
-    return selectUserPasswordRow;
-  }
-
-// 유저 계정 상태 체크 (jwt 생성 위해 id 값도 가져온다.)
-async function selectUserAccount(connection, userEmail) {
-    const selectUserAccountQuery = `
-          SELECT status, userIndex
-          FROM User
-          WHERE userEmail = ?;`;
-    const selectUserAccountRow = await connection.query(
-        selectUserAccountQuery,
-        userEmail
-    );
-    return selectUserAccountRow[0];
-  }
-
-
->>>>>>> 416e6d9f9515e632adc4b447f9dff35fc6986e03
 
 module.exports = {
     selectUserEmail,
     insertUserInfo,
-<<<<<<< HEAD
     updateUserAddress
-=======
-    selectUserPassword,
-    selectUserAccount
->>>>>>> 416e6d9f9515e632adc4b447f9dff35fc6986e03
 };
